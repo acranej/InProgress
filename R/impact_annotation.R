d9366a2cd7a384276b3dec6926371ea91baa43ef
acranej <acrane@broadinstitute.org>
2022-03-23 14:00:15 -0400

    impact initial

---
+++ R/impact_annotation.R
@@ -0,0 +1,63 @@
+
+#' Ensembl gene coordinates
+#'
+#' Gene coordinates hg38
+#' @name hg38_ensembl_genelocations
+#' @docType data
+#' @keywords data internal
+#' @format \code{data.table}
+
+hg38_ensembl_genelocations = readRDS(system.file("extdata","hg38_ensembl_genelocations_formatted.rds", package = "InProgress"))
+
+#' Ensembl exon coordinates
+#'
+#' Exon coordinates hg38
+#' @name hg38_ensembl_exonlocations
+#' @docType data
+#' @keywords data internal
+#' @format \code{data.table}
+
+hg38_ensembl_exonlocations = readRDS(system.file("extdata","hg38_ensembl_exonlocations_formatted.rds", package = "InProgress"))
+
+#' Gene overlap
+#' @name find_gene_overlap
+#' @title SV occurance in genes
+#' @param bed_g bedpe row
+#' @return \code{GRangesList} of genes
+#' @description 
+#'
+#' Determines if an SV is overlapping any genes
+#' 
+#' @import data.table
+#' @import GenomicRanges
+#' @import gUtils
+#' @keywords internal
+#' 
+
+
+find_gene_overlap = function(bed_g = NULL) {
+  
+}
+
+#' Exon overlap
+#' @name find_exon_overlap
+#' @title SV occurance in exons
+#' @param bed_e bedpe row
+#' @return \code{GRanges} of genes
+#' @description 
+#'
+#' Determines if an SV is overlapping any genes
+#' 
+#' @import data.table
+#' @import GenomicRanges
+#' @import gUtils
+#' @keywords internal
+#' 
+
+find_exon_overlap = function(bed_e = NULL) {
+  
+}
+
+
+
+
